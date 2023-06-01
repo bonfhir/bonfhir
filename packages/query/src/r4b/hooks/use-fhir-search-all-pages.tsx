b031ccb11ce6a3d47eed7f199887f56877282d2d
Julien Blin <julien@alleycorpnord.com>
2023-06-01 15:15:05 -0400

    Add searchAllPages

---
+++ packages/query/src/r4b/hooks/use-fhir-search-all-pages.tsx
@@ -0,0 +1,62 @@
+import {
+  AnyResourceType,
+  BundleNavigator,
+  ExtractResource,
+  FhirClientSearchParameters,
+  Retrieved,
+  normalizeSearchParameters,
+} from "@bonfhir/core/r4b";
+import {
+  UseQueryOptions,
+  UseQueryResult,
+  useQuery,
+} from "@tanstack/react-query";
+import { FhirQueryKeys } from "../cache-keys.js";
+import { useFhirClientQueryContext } from "../context.js";
+
+export interface UseFhirSearchAllPAgesOptions<
+  TResourceType extends AnyResourceType
+> {
+  /** The FhirClient key to use to perform the query. */
+  fhirClient?: string | null | undefined;
+  query?:
+    | Omit<
+        UseQueryOptions<
+          BundleNavigator<Retrieved<ExtractResource<TResourceType>>>,
+          unknown,
+          BundleNavigator<Retrieved<ExtractResource<TResourceType>>>,
+          ReturnType<(typeof FhirQueryKeys)["search"]>
+        >,
+        "initialData" | "queryKey" | "queryFn" | "keepPreviousData"
+      >
+    | null
+    | undefined;
+}
+
+/**
+ * Return a [Query](https://tanstack.com/query/latest/docs/react/guides/queries) for a search request.
+ *
+ * @param parameters - the initial search parameters can either be a function that manipulates a `resourceSearch` for the
+ * primary resource type or a search parameters string
+ *
+ * @see https://hl7.org/fhir/http.html#search
+ */
+export function useFhirSearchAllPages<TResourceType extends AnyResourceType>(
+  type: TResourceType,
+  parameters: FhirClientSearchParameters<TResourceType>,
+  options?: UseFhirSearchAllPAgesOptions<TResourceType> | null | undefined
+): UseQueryResult<BundleNavigator<ExtractResource<TResourceType>>> {
+  const fhirQueryContext = useFhirClientQueryContext(options?.fhirClient);
+  const normalizedParameters = normalizeSearchParameters(type, parameters);
+
+  return useQuery({
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    ...(options?.query as any),
+    queryKey: FhirQueryKeys.search(
+      fhirQueryContext.clientKey,
+      type,
+      normalizedParameters
+    ),
+    queryFn: () => fhirQueryContext.fhirClient.searchAllPages(type, parameters),
+  });
+}
