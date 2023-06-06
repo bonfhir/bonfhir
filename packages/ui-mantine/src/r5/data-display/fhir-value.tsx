2dfa0dc470e8cacd5ff764bd544ae24d64431c10
Julien Blin <julien@alleycorpnord.com>
2023-06-06 15:40:07 -0400

    Better FhirValue

--- packages/ui-mantine/src/r5/data-display/fhir-value.tsx
+++ packages/ui-mantine/src/r5/data-display/fhir-value.tsx
@@ -1,10 +1,39 @@
-import { FhirValueRendererProps } from "@bonfhir/ui/r5";
-import { Text, TextProps } from "@mantine/core";
+import type { CommonFormatterOptions, valueFormatters } from "@bonfhir/core/r5";
+import { FhirValueRendererProps, useFhirUIContext } from "@bonfhir/ui/r5";
+import {
+  HoverCard,
+  HoverCardDropdownProps,
+  HoverCardProps,
+  HoverCardTargetProps,
+  Text,
+  TextProps,
+} from "@mantine/core";
 import { ReactElement } from "react";
 
 export function MantineFhirValue(
   props: FhirValueRendererProps<MantineFhirValueProps>
 ): ReactElement | null {
+  const { formatter } = useFhirUIContext();
+  if (hasRelativeOptions(props.options)) {
+    return (
+      <HoverCard openDelay={1000} {...props.rendererProps?.hoverCard}>
+        <HoverCard.Target {...props.rendererProps?.hoverCardTarget}>
+          <Text span {...props.rendererProps?.text}>
+            {props.formattedValue}
+          </Text>
+        </HoverCard.Target>
+        <HoverCard.Dropdown {...props.rendererProps?.hoverCardDropdown}>
+          <Text span {...props.rendererProps?.text}>
+            {formatter.format(
+              props.type,
+              props.value as never,
+              props.rendererProps?.relativeDateHoverCardFormatter
+            )}
+          </Text>
+        </HoverCard.Dropdown>
+      </HoverCard>
+    );
+  }
   return (
     <Text span {...props.rendererProps?.text}>
       {props.formattedValue}
@@ -14,4 +43,17 @@ export function MantineFhirValue(
 
 export interface MantineFhirValueProps {
   text?: TextProps | null | undefined;
+  hoverCard?: HoverCardProps | null | undefined;
+  hoverCardTarget?: HoverCardTargetProps | null | undefined;
+  hoverCardDropdown?: HoverCardDropdownProps | null | undefined;
+  relativeDateHoverCardFormatter?:
+    | (valueFormatters.DatetimeFormatterOptions & CommonFormatterOptions)
+    | null
+    | undefined;
+}
+
+function hasRelativeOptions(
+  value: FhirValueRendererProps<MantineFhirValueProps>["options"]
+): value is { dateStyle: "relative" } {
+  return !!value && "dateStyle" in value && value.dateStyle === "relative";
 }
