6703e8d74804fc62eebabb5935de7be82a3bf999
Julien Blin <julien@alleycorpnord.com>
2023-08-14 09:24:18 -0400

    Add support for global browser import

---
+++ packages/core/src/r4b/global.ts
@@ -0,0 +1,48 @@
+/**
+ * This module exists as an alternative entry-point to be able to directly reference the core library
+ * in a global import context, such as a browser.
+ * This enables usage in third-party tools such as no-code tools.
+ */
+
+import * as builders from "./builders";
+import * as bundleExecutor from "./bundle-executor";
+import * as bundleNavigator from "./bundle-navigator";
+import * as dateTimeHelpers from "./date-time-helpers";
+import * as dedupSearch from "./dedup-search";
+import * as extensions from "./extensions";
+import * as fetchFhirClient from "./fetch-fhir-client";
+import * as fhirClient from "./fhir-client";
+import * as fhirTypes from "./fhir-types.codegen";
+import * as formatters from "./formatters";
+import * as langUtils from "./lang-utils";
+import * as merge from "./merge";
+import * as narratives from "./narratives.codegen";
+import * as operations from "./operations.codegen";
+import * as patch from "./patch";
+import * as patchCodegen from "./patch.codegen";
+import * as references from "./references.codegen";
+import * as search from "./search";
+import * as searchCodegen from "./search.codegen";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+(globalThis as any).bonfhir = {
+  ...builders,
+  ...bundleExecutor,
+  ...bundleNavigator,
+  ...dateTimeHelpers,
+  ...dedupSearch,
+  ...extensions,
+  ...fetchFhirClient,
+  ...fhirClient,
+  ...fhirTypes,
+  ...formatters,
+  ...langUtils,
+  ...merge,
+  ...narratives,
+  ...operations,
+  ...patchCodegen,
+  ...patch,
+  ...references,
+  ...searchCodegen,
+  ...search,
+};
