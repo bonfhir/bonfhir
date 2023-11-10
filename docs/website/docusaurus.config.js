5e26b69275a6f65870b298ee7185182208c55619
Julien Blin <julien@alleycorpnord.com>
2023-11-10 09:39:33 -0500

    Upgrade docusaurus to v3

--- docs/website/docusaurus.config.js
+++ docs/website/docusaurus.config.js
@@ -1,8 +1,7 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require("prism-react-renderer/themes/dracula");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const { themes } = require("prism-react-renderer");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -78,8 +77,8 @@ const config = {
         ],
       },
       prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
+        theme: themes.dracula,
+        darkTheme: themes.dracula,
       },
       colorMode: {
         disableSwitch: true,
