534a788250bab14d33cf59ab76070606c24b08a1
Raphael Souza <raphael.pinheiro.souza@gmail.com>
2024-01-08 15:29:42 -0500

    build with rollup

--- packages/n8n/rollup.config.mjs
+++ packages/n8n/rollup.config.mjs
@@ -1,12 +1,22 @@
 import commonjs from "@rollup/plugin-commonjs";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
+import { glob } from "glob";
+import path from "node:path";
+import { URL, fileURLToPath } from "node:url";
 
 export default {
-  input: [
-    "nodes/Fhir/ResourceLookup.node.ts",
-    "nodes/FHIRWebhookTrigger/FHIRWebhookTrigger.node.ts",
-  ],
+  input: Object.fromEntries(
+    glob
+      .sync(["credentials/**/*.credentials.ts", "nodes/**/*.node.ts"])
+      .map((file) => [
+        path.relative(
+          "",
+          file.slice(0, file.length - path.extname(file).length),
+        ),
+        fileURLToPath(new URL(file, import.meta.url)),
+      ]),
+  ),
   output: [
     {
       dir: "dist",
