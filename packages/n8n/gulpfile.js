aa357d2377325f881fe15d7c7c9b133ca45d7b0f
Raphael Souza <raphael.pinheiro.souza@gmail.com>
2024-01-08 15:29:42 -0500

    build with rollup

--- packages/n8n/gulpfile.js
+++ packages/n8n/gulpfile.js
@@ -1,16 +1,16 @@
-const path = require('path');
-const { task, src, dest } = require('gulp');
+const path = require("node:path");
+const { task, src, dest } = require("gulp");
 
-task('build:icons', copyIcons);
+task("build:icons", copyIcons);
 
 function copyIcons() {
-	const nodeSource = path.resolve('nodes', '**', '*.{png,svg}');
-	const nodeDestination = path.resolve('dist', 'nodes');
+  const nodeSource = path.resolve("nodes", "**", "*.{png,svg,json}");
+  const nodeDestination = path.resolve("dist", "nodes");
 
-	src(nodeSource).pipe(dest(nodeDestination));
+  src(nodeSource).pipe(dest(nodeDestination));
 
-	const credSource = path.resolve('credentials', '**', '*.{png,svg}');
-	const credDestination = path.resolve('dist', 'credentials');
+  const credSource = path.resolve("credentials", "**", "*.{png,svg,json}");
+  const credDestination = path.resolve("dist", "credentials");
 
-	return src(credSource).pipe(dest(credDestination));
+  return src(credSource).pipe(dest(credDestination));
 }
