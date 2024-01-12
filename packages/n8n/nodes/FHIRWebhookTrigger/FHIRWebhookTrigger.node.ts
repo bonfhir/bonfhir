ab001fb053b7a10b68d2068a7054fc20a35105ee
Raphael Souza <raphael.pinheiro.souza@gmail.com>
2024-01-12 12:59:56 -0500

    fix parsing and make resource criteria dynamic

--- packages/n8n/nodes/FHIRWebhookTrigger/FHIRWebhookTrigger.node.ts
+++ packages/n8n/nodes/FHIRWebhookTrigger/FHIRWebhookTrigger.node.ts
@@ -1,11 +1,13 @@
 import { IHookFunctions, IWebhookFunctions } from "n8n-core";
 import {
+  IDataObject,
   INodeType,
   INodeTypeDescription,
   IWebhookResponseData,
 } from "n8n-workflow";
 
 import { FetchFhirClient, FhirClient, build } from "@bonfhir/core/r4b";
+import { resourceTypes } from "./Resources";
 
 export class FHIRWebhookTrigger implements INodeType {
   description: INodeTypeDescription = {
@@ -21,13 +23,38 @@ export class FHIRWebhookTrigger implements INodeType {
     },
     inputs: [],
     outputs: ["main"],
-    properties: [],
+    properties: [
+      {
+        displayName: "Additional Fields",
+        name: "additionalFields",
+        type: "collection",
+        placeholder: "Add Field",
+        default: {},
+        options: [
+          {
+            displayName: "Override Webhook URL",
+            name: "overrideWebhookUrl",
+            type: "string",
+            default: "",
+          },
+        ],
+      },
+      {
+        displayName: "Resource",
+        name: "resource",
+        type: "options",
+        default: "patient",
+        options: resourceTypes,
+        required: true,
+        description: "The resource type to listen for.",
+      },
+    ],
     webhooks: [
       {
         name: "default",
         httpMethod: "POST",
         responseMode: "onReceived",
-        path: "",
+        path: "webhook",
       },
     ],
     credentials: [
@@ -40,8 +67,12 @@ export class FHIRWebhookTrigger implements INodeType {
 
   async webhook(this: IWebhookFunctions): Promise<IWebhookResponseData> {
     const req = this.getRequestObject();
+
+    const rawResource = Buffer.from(req.rawBody);
+    const resource = JSON.parse(rawResource.toString());
+
     return {
-      workflowData: [this.helpers.returnJsonArray(req.body)],
+      workflowData: [this.helpers.returnJsonArray(resource)],
     };
   }
 
@@ -67,9 +98,17 @@ export class FHIRWebhookTrigger implements INodeType {
       },
 
       async create(this: IHookFunctions): Promise<boolean> {
-        const webhookUrl = this.getNodeWebhookUrl("default");
+        const defaultWebhookUrl = this.getNodeWebhookUrl("default");
         const webhookData = this.getWorkflowStaticData("node");
 
+        const additionalFields = this.getNodeParameter(
+          "additionalFields",
+        ) as IDataObject;
+
+        const webhookUrl =
+          additionalFields.overrideWebhookUrl || defaultWebhookUrl;
+
+        const resource = this.getNodeParameter("resource") as string;
         const credentials = await this.getCredentials("FhirOAuth2Api");
         const client: FhirClient = new FetchFhirClient({
           baseUrl: "http://localhost:8103/fhir/R4",
@@ -81,10 +120,11 @@ export class FHIRWebhookTrigger implements INodeType {
           build("Subscription", {
             status: "active",
             reason: "n8n workflow trigger",
-            criteria: "Patient",
+            criteria: resource[0].toUpperCase() + resource.slice(1),
             channel: {
               type: "rest-hook",
-              endpoint: webhookUrl,
+              endpoint: webhookUrl as string,
+              payload: "application/fhir+json",
             },
           }),
         );
---
+++ packages/n8n/nodes/FHIRWebhookTrigger/Resources.ts
@@ -0,0 +1,576 @@
+import { INodePropertyOptions } from "n8n-workflow";
+
+export const resourceTypes: INodePropertyOptions[] = [
+  {
+    name: "Account",
+    value: "account",
+  },
+  {
+    name: "ActivityDefinition",
+    value: "activitydefinition",
+  },
+  {
+    name: "AdministrableProductDefinition",
+    value: "administrableproductdefinition",
+  },
+  {
+    name: "AdverseEvent",
+    value: "adverseevent",
+  },
+  {
+    name: "AllergyIntolerance",
+    value: "allergyintolerance",
+  },
+  {
+    name: "Appointment",
+    value: "appointment",
+  },
+  {
+    name: "AppointmentResponse",
+    value: "appointmentresponse",
+  },
+  {
+    name: "AuditEvent",
+    value: "auditevent",
+  },
+  {
+    name: "Basic",
+    value: "basic",
+  },
+  {
+    name: "Binary",
+    value: "binary",
+  },
+  {
+    name: "BiologicallyDerivedProduct",
+    value: "biologicallyderivedproduct",
+  },
+  {
+    name: "BodyStructure",
+    value: "bodystructure",
+  },
+  {
+    name: "Bundle",
+    value: "bundle",
+  },
+  {
+    name: "CapabilityStatement",
+    value: "capabilitystatement",
+  },
+  {
+    name: "CarePlan",
+    value: "careplan",
+  },
+  {
+    name: "CareTeam",
+    value: "careteam",
+  },
+  {
+    name: "CatalogEntry",
+    value: "catalogentry",
+  },
+  {
+    name: "ChargeItem",
+    value: "chargeitem",
+  },
+  {
+    name: "ChargeItemDefinition",
+    value: "chargeitemdefinition",
+  },
+  {
+    name: "Citation",
+    value: "citation",
+  },
+  {
+    name: "Claim",
+    value: "claim",
+  },
+  {
+    name: "ClaimResponse",
+    value: "claimresponse",
+  },
+  {
+    name: "ClinicalImpression",
+    value: "clinicalimpression",
+  },
+  {
+    name: "ClinicalUseDefinition",
+    value: "clinicalusedefinition",
+  },
+  {
+    name: "CodeSystem",
+    value: "codesystem",
+  },
+  {
+    name: "Communication",
+    value: "communication",
+  },
+  {
+    name: "CommunicationRequest",
+    value: "communicationrequest",
+  },
+  {
+    name: "CompartmentDefinition",
+    value: "compartmentdefinition",
+  },
+  {
+    name: "Composition",
+    value: "composition",
+  },
+  {
+    name: "ConceptMap",
+    value: "conceptmap",
+  },
+  {
+    name: "Condition",
+    value: "condition",
+  },
+  {
+    name: "Consent",
+    value: "consent",
+  },
+  {
+    name: "Contract",
+    value: "contract",
+  },
+  {
+    name: "Coverage",
+    value: "coverage",
+  },
+  {
+    name: "CoverageEligibilityRequest",
+    value: "coverageeligibilityrequest",
+  },
+  {
+    name: "CoverageEligibilityResponse",
+    value: "coverageeligibilityresponse",
+  },
+  {
+    name: "DetectedIssue",
+    value: "detectedissue",
+  },
+  {
+    name: "Device",
+    value: "device",
+  },
+  {
+    name: "DeviceDefinition",
+    value: "devicedefinition",
+  },
+  {
+    name: "DeviceMetric",
+    value: "devicemetric",
+  },
+  {
+    name: "DeviceRequest",
+    value: "devicerequest",
+  },
+  {
+    name: "DeviceUseStatement",
+    value: "deviceusestatement",
+  },
+  {
+    name: "DiagnosticReport",
+    value: "diagnosticreport",
+  },
+  {
+    name: "DocumentManifest",
+    value: "documentmanifest",
+  },
+  {
+    name: "DocumentReference",
+    value: "documentreference",
+  },
+  {
+    name: "DomainResource",
+    value: "domainresource",
+  },
+  {
+    name: "Encounter",
+    value: "encounter",
+  },
+  {
+    name: "Endpoint",
+    value: "endpoint",
+  },
+  {
+    name: "EnrollmentRequest",
+    value: "enrollmentrequest",
+  },
+  {
+    name: "EnrollmentResponse",
+    value: "enrollmentresponse",
+  },
+  {
+    name: "EpisodeOfCare",
+    value: "episodeofcare",
+  },
+  {
+    name: "EventDefinition",
+    value: "eventdefinition",
+  },
+  {
+    name: "Evidence",
+    value: "evidence",
+  },
+  {
+    name: "EvidenceReport",
+    value: "evidencereport",
+  },
+  {
+    name: "EvidenceVariable",
+    value: "evidencevariable",
+  },
+  {
+    name: "ExampleScenario",
+    value: "examplescenario",
+  },
+  {
+    name: "ExplanationOfBenefit",
+    value: "explanationofbenefit",
+  },
+  {
+    name: "FamilyMemberHistory",
+    value: "familymemberhistory",
+  },
+  {
+    name: "Flag",
+    value: "flag",
+  },
+  {
+    name: "Goal",
+    value: "goal",
+  },
+  {
+    name: "GraphDefinition",
+    value: "graphdefinition",
+  },
+  {
+    name: "Group",
+    value: "group",
+  },
+  {
+    name: "GuidanceResponse",
+    value: "guidanceresponse",
+  },
+  {
+    name: "HealthcareService",
+    value: "healthcareservice",
+  },
+  {
+    name: "ImagingStudy",
+    value: "imagingstudy",
+  },
+  {
+    name: "Immunization",
+    value: "immunization",
+  },
+  {
+    name: "ImmunizationEvaluation",
+    value: "immunizationevaluation",
+  },
+  {
+    name: "ImmunizationRecommendation",
+    value: "immunizationrecommendation",
+  },
+  {
+    name: "ImplementationGuide",
+    value: "implementationguide",
+  },
+  {
+    name: "Ingredient",
+    value: "ingredient",
+  },
+  {
+    name: "InsurancePlan",
+    value: "insuranceplan",
+  },
+  {
+    name: "Invoice",
+    value: "invoice",
+  },
+  {
+    name: "Library",
+    value: "library",
+  },
+  {
+    name: "Linkage",
+    value: "linkage",
+  },
+  {
+    name: "List",
+    value: "list",
+  },
+  {
+    name: "Location",
+    value: "location",
+  },
+  {
+    name: "ManufacturedItemDefinition",
+    value: "manufactureditemdefinition",
+  },
+  {
+    name: "Measure",
+    value: "measure",
+  },
+  {
+    name: "MeasureReport",
+    value: "measurereport",
+  },
+  {
+    name: "Media",
+    value: "media",
+  },
+  {
+    name: "Medication",
+    value: "medication",
+  },
+  {
+    name: "MedicationAdministration",
+    value: "medicationadministration",
+  },
+  {
+    name: "MedicationDispense",
+    value: "medicationdispense",
+  },
+  {
+    name: "MedicationKnowledge",
+    value: "medicationknowledge",
+  },
+  {
+    name: "MedicationRequest",
+    value: "medicationrequest",
+  },
+  {
+    name: "MedicationStatement",
+    value: "medicationstatement",
+  },
+  {
+    name: "MedicinalProductDefinition",
+    value: "medicinalproductdefinition",
+  },
+  {
+    name: "MessageDefinition",
+    value: "messagedefinition",
+  },
+  {
+    name: "MessageHeader",
+    value: "messageheader",
+  },
+  {
+    name: "MolecularSequence",
+    value: "molecularsequence",
+  },
+  {
+    name: "NamingSystem",
+    value: "namingsystem",
+  },
+  {
+    name: "NutritionOrder",
+    value: "nutritionorder",
+  },
+  {
+    name: "NutritionProduct",
+    value: "nutritionproduct",
+  },
+  {
+    name: "Observation",
+    value: "observation",
+  },
+  {
+    name: "ObservationDefinition",
+    value: "observationdefinition",
+  },
+  {
+    name: "OperationDefinition",
+    value: "operationdefinition",
+  },
+  {
+    name: "OperationOutcome",
+    value: "operationoutcome",
+  },
+  {
+    name: "Organization",
+    value: "organization",
+  },
+  {
+    name: "OrganizationAffiliation",
+    value: "organizationaffiliation",
+  },
+  {
+    name: "PackagedProductDefinition",
+    value: "packagedproductdefinition",
+  },
+  {
+    name: "Parameters",
+    value: "parameters",
+  },
+  {
+    name: "Patient",
+    value: "patient",
+  },
+  {
+    name: "PaymentNotice",
+    value: "paymentnotice",
+  },
+  {
+    name: "PaymentReconciliation",
+    value: "paymentreconciliation",
+  },
+  {
+    name: "Person",
+    value: "person",
+  },
+  {
+    name: "PlanDefinition",
+    value: "plandefinition",
+  },
+  {
+    name: "Practitioner",
+    value: "practitioner",
+  },
+  {
+    name: "PractitionerRole",
+    value: "practitionerrole",
+  },
+  {
+    name: "Procedure",
+    value: "procedure",
+  },
+  {
+    name: "Provenance",
+    value: "provenance",
+  },
+  {
+    name: "Questionnaire",
+    value: "questionnaire",
+  },
+  {
+    name: "QuestionnaireResponse",
+    value: "questionnaireresponse",
+  },
+  {
+    name: "RegulatedAuthorization",
+    value: "regulatedauthorization",
+  },
+  {
+    name: "RelatedPerson",
+    value: "relatedperson",
+  },
+  {
+    name: "RequestGroup",
+    value: "requestgroup",
+  },
+  {
+    name: "ResearchDefinition",
+    value: "researchdefinition",
+  },
+  {
+    name: "ResearchElementDefinition",
+    value: "researchelementdefinition",
+  },
+  {
+    name: "ResearchStudy",
+    value: "researchstudy",
+  },
+  {
+    name: "ResearchSubject",
+    value: "researchsubject",
+  },
+  {
+    name: "Resource",
+    value: "resource",
+  },
+  {
+    name: "RiskAssessment",
+    value: "riskassessment",
+  },
+  {
+    name: "Schedule",
+    value: "schedule",
+  },
+  {
+    name: "SearchParameter",
+    value: "searchparameter",
+  },
+  {
+    name: "ServiceRequest",
+    value: "servicerequest",
+  },
+  {
+    name: "Slot",
+    value: "slot",
+  },
+  {
+    name: "Specimen",
+    value: "specimen",
+  },
+  {
+    name: "SpecimenDefinition",
+    value: "specimendefinition",
+  },
+  {
+    name: "StructureDefinition",
+    value: "structuredefinition",
+  },
+  {
+    name: "StructureMap",
+    value: "structuremap",
+  },
+  {
+    name: "Subscription",
+    value: "subscription",
+  },
+  {
+    name: "SubscriptionStatus",
+    value: "subscriptionstatus",
+  },
+  {
+    name: "SubscriptionTopic",
+    value: "subscriptiontopic",
+  },
+  {
+    name: "Substance",
+    value: "substance",
+  },
+  {
+    name: "SubstanceDefinition",
+    value: "substancedefinition",
+  },
+  {
+    name: "SupplyDelivery",
+    value: "supplydelivery",
+  },
+  {
+    name: "SupplyRequest",
+    value: "supplyrequest",
+  },
+  {
+    name: "Task",
+    value: "task",
+  },
+  {
+    name: "TerminologyCapabilities",
+    value: "terminologycapabilities",
+  },
+  {
+    name: "TestReport",
+    value: "testreport",
+  },
+  {
+    name: "TestScript",
+    value: "testscript",
+  },
+  {
+    name: "ValueSet",
+    value: "valueset",
+  },
+  {
+    name: "VerificationResult",
+    value: "verificationresult",
+  },
+  {
+    name: "VisionPrescription",
+    value: "visionprescription",
+  },
+];
