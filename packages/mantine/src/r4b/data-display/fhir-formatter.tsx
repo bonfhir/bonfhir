26ef8b917325a49faa9b14876441199f42bc37eb
ysael <me@ysael.com>
2024-01-18 08:30:34 -0500

    r4

--- packages/mantine/src/r4b/data-display/fhir-formatter.tsx
+++ packages/mantine/src/r4b/data-display/fhir-formatter.tsx
@@ -2,7 +2,7 @@ import { FhirFormatterRendererProps } from "@bonfhir/react/r4b";
 import { ReactElement } from "react";
 
 export function MantineFhirFormatter(
-  props: FhirFormatterRendererProps<unknown>,
+  props: FhirFormatterRendererProps<unknown>
 ): ReactElement | null {
   return <>{props.formattedValue}</>;
 }
