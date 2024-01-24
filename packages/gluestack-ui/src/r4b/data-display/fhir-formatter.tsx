d6694549718950e3503b9c6a2fe40fc7c2b144bd
Nick Vallee <nickvallee@gmail.com>
2024-01-24 16:14:40 -0500

    Create Gluestack-ui renderer for FhirFormatter

---
+++ packages/gluestack-ui/src/r4b/data-display/fhir-formatter.tsx
@@ -0,0 +1,15 @@
+import { FhirFormatterRendererProps } from "@bonfhir/react/r4b";
+import { Text } from "@gluestack-ui/themed";
+import { ComponentProps, ReactElement } from "react";
+
+type TextProps = ComponentProps<typeof Text>;
+
+export function GlueStackFhirFormatter(
+  props: FhirFormatterRendererProps<GlueStackFhirFormatter>,
+): ReactElement | null {
+  return <Text {...props.rendererProps?.text}>{props.formattedValue}</Text>;
+}
+
+export interface GlueStackFhirFormatter {
+  text?: TextProps | null | undefined;
+}
