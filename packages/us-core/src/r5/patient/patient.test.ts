c4c3e3da2cd1f76194a6d822fb2c3fd47936d115
Julien Blin <julien@alleycorpnord.com>
2024-02-08 07:48:05 -0500

    Refactor snapshot assertions in patient and questionnaire-response tests

--- packages/us-core/src/r5/patient/patient.test.ts
+++ packages/us-core/src/r5/patient/patient.test.ts
@@ -143,6 +143,129 @@ describe("patient", () => {
       }),
     );
 
-    expect(JSON.stringify(patient)).toMatchSnapshot();
+    expect(patient).toMatchObject({
+      extension: [
+        {
+          extension: [
+            {
+              url: "ombCategory",
+              valueCoding: {
+                code: "2106-3",
+                display: "White",
+                system: "urn:oid:2.16.840.1.113883.6.238",
+              },
+            },
+            {
+              url: "ombCategory",
+              valueCoding: {
+                code: "2028-9",
+                display: "Asian",
+                system: "urn:oid:2.16.840.1.113883.6.238",
+              },
+            },
+            {
+              url: "detailed",
+              valueCoding: {
+                code: "1586-7",
+                display: "Shoshone",
+                system: "urn:oid:2.16.840.1.113883.6.238",
+              },
+            },
+            {
+              url: "detailed",
+              valueCoding: {
+                code: "2036-2",
+                display: "Filipino",
+                system: "urn:oid:2.16.840.1.113883.6.238",
+              },
+            },
+            {
+              url: "text",
+              valueString: "Mixed",
+            },
+          ],
+          url: "http://hl7.org/fhir/us/core/StructureDefinition/us-core-race",
+        },
+        {
+          extension: [
+            {
+              url: "ombCategory",
+              valueCoding: {
+                code: "2135-2",
+                display: "Hispanic or Latino",
+                system: "urn:oid:2.16.840.1.113883.6.238",
+              },
+            },
+            {
+              url: "detailed",
+              valueCoding: {
+                code: "2184-0",
+                display: "Dominican",
+                system: "urn:oid:2.16.840.1.113883.6.238",
+              },
+            },
+            {
+              url: "detailed",
+              valueCoding: {
+                code: "2148-5",
+                display: "Mexican",
+                system: "urn:oid:2.16.840.1.113883.6.238",
+              },
+            },
+            {
+              url: "text",
+              valueString: "Hispanic or Latino",
+            },
+          ],
+          url: "http://hl7.org/fhir/us/core/StructureDefinition/us-core-ethnicity",
+        },
+        {
+          extension: [
+            {
+              url: "tribalAffiliation",
+              valueCodeableConcept: {
+                coding: [
+                  {
+                    code: "187",
+                    display:
+                      "Paiute-Shoshone Tribe of the Fallon Reservation and Colony, Nevada",
+                    system:
+                      "http://terminology.hl7.org/CodeSystem/v3-TribalEntityUS",
+                  },
+                ],
+                text: "Paiute-Shoshone Tribe of the Fallon Reservation and Colony, Nevada",
+              },
+            },
+            {
+              url: "isEnrolled",
+              valueBoolean: false,
+            },
+          ],
+          url: "http://hl7.org/fhir/us/core/StructureDefinition/us-core-tribal-affiliation",
+        },
+        {
+          url: "http://hl7.org/fhir/us/core/StructureDefinition/us-core-birthsex",
+          valueCode: "F",
+        },
+        {
+          url: "http://hl7.org/fhir/us/core/StructureDefinition/us-core-sex",
+          valueCode: "184115007",
+        },
+        {
+          url: "http://hl7.org/fhir/us/core/StructureDefinition/us-core-genderIdentity",
+          valueCodeableConcept: {
+            coding: [
+              {
+                code: "UNK",
+                display: "Unknown",
+                system: "http://terminology.hl7.org/CodeSystem/v3-NullFlavor",
+              },
+            ],
+            text: "Unknown",
+          },
+        },
+      ],
+      resourceType: "Patient",
+    });
   });
 });
