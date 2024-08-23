748ca7f9b2db5b80a0eb13c3b435baae0afa7b0b
Mireille Trempe <mireille@alleycorpnord.com>
2024-08-23 14:44:38 -0400

    feat: add formatters context in r5

--- packages/react/src/r5/context.ts
+++ packages/react/src/r5/context.ts
@@ -1,9 +1,12 @@
 import { DefaultFormatter } from "@bonfhir/core/r5";
-import { ReactElement, createContext, useContext } from "react";
+import { ReactElement, createContext } from "react";
 import { FhirUIDefaultProps } from "./default-props";
 import { FhirUIRenderer } from "./renderer";
 
 export interface FhirUIContext {
+  /**
+   * @deprecated formatter in UI context is deprecated: please @see FhirFormatterProvider and its hook @see useFhirFormatters
+   */
   formatter: DefaultFormatter;
   renderer: Partial<FhirUIRenderer>;
   defaultProps?: FhirUIDefaultProps | null | undefined;
@@ -21,25 +24,7 @@ export interface FhirUIContext {
 /**
  * The context used by FHIR UI Components.
  */
-export const FhirUIContext = createContext<FhirUIContext | undefined>(
-  undefined,
-);
-
-/**
- * Get the current {@link FhirUIContext}.
- *
- * @throws Error if no parent context exists (a.k.a. no `FhirUIProvider` was used in the parent tree).
- */
-export const useFhirUIContext = (): FhirUIContext => {
-  const context = useContext(FhirUIContext);
-  if (!context) {
-    throw new Error(
-      "Missing FhirUIContext. Did you forget to use a parent FhirUIProvider?",
-    );
-  }
-
-  return context;
-};
+export const FhirUIContext = createContext<FhirUIContext>({} as FhirUIContext);
 
 export interface OnNavigateArgs {
   target: string;
---
+++ packages/react/src/r5/formatters/provider.tsx
@@ -0,0 +1,61 @@
+import { Formatter, FormatterOptions } from "@bonfhir/core/r5";
+import { useMemo } from "react";
+import { FhirFormattersContext } from "./fhir-formatters-context";
+
+export type FhirFormattersProps = {
+  formatters?: Formatter;
+  options?: FormatterOptions;
+  children?: React.ReactNode | React.ReactNode[];
+};
+
+/**
+ * FhirFormatterProvider: Primary provider for value formatters.
+ * Should be placed at the root of your React app if possible, to make your whole app use the same source of formatters.
+ * @param formatters you can provide your own formatters implementations, OR
+ * @param options you can specify culture (locale), delimiter, etc. without re-implementing the whole thing
+ * @param children React descendants
+ *
+ * @example
+ * export const BaseLayout: React.FC = () => {
+ *
+ *   return (
+ *     <FhirFormatterProvider>
+ *       ...
+ *     </FhirFormatterProvider>
+ *   );
+ * }
+ *
+ * @example
+ * export const BaseLayout: React.FC = () => {
+ *   const [locale, setLocale] = useState<'en' | 'es' | 'fr'>('en');
+ *
+ *   const handleOnLocaleSwitch = (changedTo: 'en' | 'es' | 'fr') => {
+ *     setLocale(changedTo);
+ *   };
+ *
+ *   return (
+ *     <FhirFormatterProvider options={{ locale }}>
+ *       <button onClick={() => handleOnLocaleSwitch('en')}>EN</button>
+ *       <button onClick={() => handleOnLocaleSwitch('es')}>ES</button>
+ *       <button onClick={() => handleOnLocaleSwitch('fr')}>FR</button>
+ *       ...
+ *     </FhirFormatterProvider>
+ *   );
+ * }
+ */
+export const FhirFormatterProvider: React.FC<FhirFormattersProps> = ({
+  formatters,
+  options,
+  children,
+}) => {
+  const wrapped = useMemo(
+    () => formatters ?? Formatter.build(options),
+    [formatters, options],
+  );
+
+  return (
+    <FhirFormattersContext.Provider value={wrapped}>
+      {children}
+    </FhirFormattersContext.Provider>
+  );
+};
