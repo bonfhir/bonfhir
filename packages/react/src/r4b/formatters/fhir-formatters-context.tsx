59153e94c6404982fe0d4ee58f3b8176ee872d3e
Mireille Trempe <mireille@alleycorpnord.com>
2024-08-23 14:41:52 -0400

    feat: add formatters context in r4b

---
+++ packages/react/src/r4b/formatters/fhir-formatters-context.tsx
@@ -0,0 +1,22 @@
+import { Formatter } from "@bonfhir/core/r4b";
+import { createContext, useContext } from "react";
+
+type FhirFormatters = Formatter; // redirecting in case we add strictly react-functionalities
+
+export const FhirFormattersContext = createContext<FhirFormatters>(
+  {} as FhirFormatters,
+);
+
+/**
+ * @returns the value formatters available for FHIR values
+ * @example
+ * const formatters = useFhirFormatters();
+ *
+ * const totalCosts = formatters.format('Money', costs);
+ *
+ * @example
+ * const { format } = useFhirFormatters();
+ *
+ * const totalCosts = format('Money', costs);
+ */
+export const useFhirFormatters = () => useContext(FhirFormattersContext);
